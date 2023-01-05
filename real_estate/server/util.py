3e6901964e336decb3ad657a67a02329fdb91d99
atwanzireT <tatwanzire@gmail.com>
2023-01-05 11:47:46 +0300

    loading the model pickle file

--- real_estate/server/util.py
+++ real_estate/server/util.py
@@ -1,10 +1,26 @@
 import json
 import pickle
+import numpy
 
 __loactions = None
 __data_columns = None
 __model = None
 
+
+def get_estimated_price(location,sqft,bhk,bath):
+    try:
+        loc_index = __data_columns.index(location.lower())
+    except:
+        loc_index = -1
+
+    x = np.zeros(len(__data_columns))
+    x[0] = sqft
+    x[1] = bath
+    x[2] = bhk
+    if loc_index>=0:
+        x[loc_index] = 1
+
+    return round(__model.predict([x])[0],2)
 
 def get_location_names():
     return __loactions
@@ -23,7 +39,13 @@ def load_saved_artifacts():
         __model = pickle.load(f)
     print("Loading the artifacts is done.")
 
+def get_data_columns():
+    return __data_columns
 
-if "__main__" == __name__:
+if __name__ == '__main__':
     load_saved_artifacts()
     print(get_location_names())
+    print(get_estimated_price('1st Phase JP Nagar',1000, 3, 3))
+    print(get_estimated_price('1st Phase JP Nagar', 1000, 2, 2))
+    print(get_estimated_price('Kalhalli', 1000, 2, 2)) # other location
+    print(get_estimated_price('Ejipura', 1000, 2, 2))  # other location
